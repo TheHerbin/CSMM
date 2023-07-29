93ec9d2f10dc25c3715dd8634111e1c9e08987a0
Theo Herbin <theo.herbin59710@outlook.fr>
2023-07-29 23:06:24 +0200

    mod class init

---
+++ src/main/java/fr/Gyos/mc_mod.java
@@ -0,0 +1,25 @@
+package fr.Gyos;
+
+import net.minecraftforge.fml.common.Mod;
+import net.minecraftforge.fml.event.lifecycle.FMLClientSetupEvent;
+import net.minecraftforge.fml.event.lifecycle.FMLCommonSetupEvent;
+import net.minecraftforge.fml.javafmlmod.FMLJavaModLoadingContext;
+
+@Mod(mc_mod.MODID)
+public class mc_mod {
+    public static final String MODID = "mc_mod";
+
+    public mc_mod() {
+        FMLJavaModLoadingContext.get().getModEventBus().addListener(this::setup);
+        FMLJavaModLoadingContext.get().getModEventBus().addListener(this::clientSetup);
+    }
+
+    private void setup(FMLCommonSetupEvent e){
+
+    }
+
+    private void clientSetup(FMLClientSetupEvent e){
+
+    }
+
+}
