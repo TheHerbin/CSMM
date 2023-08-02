1593065969d05eaf13335005b99bccb8abee5d09
Theo Herbin <theo.herbin59710@outlook.fr>
2023-08-03 00:24:13 +0200

    added sword

--- src/main/java/fr/Gyos/mcmod/item/ModItems.java
+++ src/main/java/fr/Gyos/mcmod/item/ModItems.java
@@ -1,11 +1,20 @@
 package fr.Gyos.mcmod.item;
 
 import fr.Gyos.mcmod.mcmod;
-import net.minecraft.world.item.Item;
+import net.minecraft.resources.ResourceLocation;
+import net.minecraft.tags.BlockTags;
+import net.minecraft.tags.TagKey;
+import net.minecraft.world.item.*;
+import net.minecraft.world.item.crafting.Ingredient;
+import net.minecraft.world.level.block.Block;
+import net.minecraftforge.common.ForgeTier;
+import net.minecraftforge.common.TierSortingRegistry;
 import net.minecraftforge.eventbus.api.IEventBus;
 import net.minecraftforge.registries.DeferredRegister;
 import net.minecraftforge.registries.ForgeRegistries;
 import net.minecraftforge.registries.RegistryObject;
+
+import java.util.List;
 
 public class ModItems {
     public static final DeferredRegister<Item> ITEMS =
@@ -13,6 +22,20 @@ public class ModItems {
 
     public static final RegistryObject<Item> BOBUX = ITEMS.register("bobux",
             () -> new Item(new Item.Properties()));
+
+
+    //BOBUX TIER
+    public static final TagKey<Block> BOBUX_TAG = BlockTags.create(new ResourceLocation("modid", "bobux_tag"));
+
+    public static final Tier BOBUX_TIER = TierSortingRegistry.registerTier(
+            new ForgeTier(5, 500, 15.0F, 5, 25, BOBUX_TAG, () -> Ingredient.of()),
+            new ResourceLocation("modid", "bobux_tier"),
+            List.of(Tiers.DIAMOND), List.of());
+
+    //Weapons
+    public static final RegistryObject<Item> BOBUX_SWORD = ITEMS.register("bobux_sword",
+            () -> new SwordItem(BOBUX_TIER, 8, 3.5f,new Item.Properties()));
+
 
     public static void register(IEventBus eventBus) {
         ITEMS.register(eventBus);
